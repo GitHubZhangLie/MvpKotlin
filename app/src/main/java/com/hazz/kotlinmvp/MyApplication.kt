b781f7eeddbd74869b99505a041cb86b0499eea3
GitHubZhangLie <2529223773@qq.com>
2019-03-30 11:19:43 +0800

    Initial commit

---
+++ app/src/main/java/com/hazz/kotlinmvp/MyApplication.kt
@@ -0,0 +1,123 @@
+package com.hazz.kotlinmvp
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.os.Bundle
+import android.support.multidex.MultiDex
+import android.util.Log
+import com.hazz.kotlinmvp.utils.DisplayManager
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
+import com.squareup.leakcanary.LeakCanary
+import com.squareup.leakcanary.RefWatcher
+import com.tencent.bugly.Bugly
+import com.tencent.bugly.beta.Beta
+import com.tencent.bugly.crashreport.CrashReport
+import kotlin.properties.Delegates
+
+
+/**
+ * Created by xuhao on 2017/11/16.
+ *
+ */
+
+class MyApplication : Application(){
+
+    private var refWatcher: RefWatcher? = null
+
+    companion object {
+
+        private val TAG = "MyApplication"
+
+        var context: Context by Delegates.notNull()
+            private set
+
+        fun getRefWatcher(context: Context): RefWatcher? {
+            val myApplication = context.applicationContext as MyApplication
+            return myApplication.refWatcher
+        }
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        refWatcher = setupLeakCanary()
+        initConfig()
+        DisplayManager.init(this)
+        registerActivityLifecycleCallbacks(mActivityLifecycleCallbacks)
+
+        // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
+        // 调试时，将第三个参数改为true
+        Bugly.init(this, "4a786300e0", false)
+
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        // you must install multiDex whatever tinker is installed!
+        MultiDex.install(base)
+        // 安装tinker
+        Beta.installTinker()
+    }
+
+    private fun setupLeakCanary(): RefWatcher {
+        return if (LeakCanary.isInAnalyzerProcess(this)) {
+            RefWatcher.DISABLED
+        } else LeakCanary.install(this)
+    }
+
+
+    /**
+     * 初始化配置
+     */
+    private fun initConfig() {
+
+        val formatStrategy = PrettyFormatStrategy.newBuilder()
+                .showThreadInfo(false)  // 隐藏线程信息 默认：显示
+                .methodCount(0)         // 决定打印多少行（每一行代表一个方法）默认：2
+                .methodOffset(7)        // (Optional) Hides internal method calls up to offset. Default 5
+                .tag("hao_zz")   // (Optional) Global tag for every log. Default PRETTY_LOGGER
+                .build()
+        Logger.addLogAdapter(object : AndroidLogAdapter(formatStrategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
+    }
+
+
+    private val mActivityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            Log.d(TAG, "onCreated: " + activity.componentName.className)
+        }
+
+        override fun onActivityStarted(activity: Activity) {
+            Log.d(TAG, "onStart: " + activity.componentName.className)
+        }
+
+        override fun onActivityResumed(activity: Activity) {
+
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+
+        }
+
+        override fun onActivityStopped(activity: Activity) {
+
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+
+        }
+
+        override fun onActivityDestroyed(activity: Activity) {
+            Log.d(TAG, "onDestroy: " + activity.componentName.className)
+        }
+    }
+
+
+}
